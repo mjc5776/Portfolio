a9ee81213eff6949da6cf7d5d87c1b39fa1be7ad
MCorey <mjcorey57@gmail.com>
2018-12-14 16:36:59 -0700

    Portfolio updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@ var typewriter = new Typewriter(app, {
 new Typewriter('#app', {
   strings: ['"Change the world by being yourself." – Amy Poehler', '"Wanting to be someone else is a waste of who you are." – Kurt Cobain', '"Everything you can imagine is real." – Pablo Picasso', '"Have enough courage to start and enough heart to finish." – Jessica N. S. Yourko'],
   autoStart: true,
+  loop: true,
 });
 
 
